350578740b664a81f7d9cad2b62c7b2a46a9ff4a
Prayas Srivastava <sriprayas>
2020-10-05 01:27:23 +0530

    Heap-102 : WAP to test build max heap

---
+++ src/main/java/com/heap/MaxHeapBuilder.java
@@ -0,0 +1,49 @@
+package main.java.com.heap;
+
+public class MaxHeapBuilder {
+
+    public void buildMaxHeap(int[] arr){
+        int start = arr.length/2;
+        for(int i=start; i>=0; i--){
+            doMaxHeapify(arr, i);
+        }
+    }
+
+    private void doMaxHeapify(int[] arr, int idx){
+        int largeIdx = idx;
+        int lChildIdx = (2*idx) + 1;
+        int rChildIdx = (2*idx) + 2;
+
+        if(lChildIdx<arr.length && arr[lChildIdx]>arr[idx]){
+            largeIdx = lChildIdx;
+        }
+
+        if(rChildIdx<arr.length && arr[rChildIdx]>arr[largeIdx]){
+            largeIdx = rChildIdx;
+        }
+
+        if(largeIdx!=idx){
+            int tmp = arr[idx];
+            arr[idx] = arr[largeIdx];
+            arr[largeIdx] = tmp;
+            doMaxHeapify(arr, largeIdx);
+        }
+    }
+
+    private void print(int[] arr){
+        for(int e : arr){
+            System.out.print(e + " ");
+        }
+        System.out.println("");
+    }
+
+    public static void main(String[] args){
+        MaxHeapBuilder builder = new MaxHeapBuilder();
+        int[] arr = {1,4,3,7,8,9,10};
+        System.out.println("Elements before ");
+        builder.print(arr);
+        System.out.println("Elements after ");
+        builder.buildMaxHeap(arr);
+        builder.print(arr);
+    }
+}
